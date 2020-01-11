7ae411b2486746b3ce450e539b260e16e5263a3e
Sean <seancarpenter10@gmail.com>
2020-01-11 16:10:18 -0500

    Blog to blog

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Run this from the VPS to sync the live version of the website with the master branch.
 cd ~
-if [ -d "Blog" ]; then
-    rm -rf "Blog"
+if [ -d "blog" ]; then
+    rm -rf "blog"
 fi
-git clone https://github.com/Seancarpenter/Blog
-cd ~/Blog/blog
+git clone https://github.com/Seancarpenter/blog
+cd ~/blog/blog
 hugo
 cp -r ~/Blog/blog/ /var/www/seancarpenter.io/html/
 cd ~
