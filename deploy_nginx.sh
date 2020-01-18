2655d3caad54f38a89928062a3769185fd1fc622
Sean <seancarpenter10@gmail.com>
2020-01-18 14:32:43 -0500

    Blog case changes

--- deploy_nginx.sh
+++ deploy_nginx.sh
@@ -3,6 +3,6 @@ if [ -d "blog" ]; then
     rm -rf "blog"
 fi
 git clone https://github.com/Seancarpenter/blog
-cp ~/Blog/blog.nginx /etc/nginx/sites-enabled/blog
-cp ~/Blog/blog.nginx /etc/nginx/sites-available/blog
+cp ~/blog/blog.nginx /etc/nginx/sites-enabled/blog
+cp ~/blog/blog.nginx /etc/nginx/sites-available/blog
 sudo service nginx restart
